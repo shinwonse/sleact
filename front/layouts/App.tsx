50fee63f8c3002c43ebcd249c6d05880bf9c63c3
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-04-13 15:28:39 +0900

    page 추가

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router';
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
-  return <div>좀만 참아주세요!</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
