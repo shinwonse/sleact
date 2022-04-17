38b27fd12219aea5659edb76c34a17d825f6c0b1
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-04-18 00:04:20 +0900

    회원가입 페이지 수정

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -37,7 +37,7 @@ const SignUp = () => {
         setSignUpError('');
         setSignUpSuccess(false);
         axios
-          .post('/api/users', {
+          .post('http://localhost:3095/api/users', {
             email,
             nickname,
             password,
