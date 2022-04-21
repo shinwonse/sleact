69868588454eadb247d2dbd38f4aa4e022eb69d6
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-04-21 17:38:26 +0900

    리팩토링

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -3,6 +3,18 @@ import axios from 'axios';
 import React, { FC, useCallback } from 'react';
 import { Redirect } from 'react-router';
 import useSWR from 'swr';
+import {
+  Channels,
+  Chats,
+  Header,
+  MenuScroll,
+  ProfileImg,
+  RightMenu,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from './styles';
+import gravatar from 'gravatar';
 
 const Workspace: FC = ({ children }) => {
   const { data, error, revalidate, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
@@ -23,7 +35,22 @@ const Workspace: FC = ({ children }) => {
 
   return (
     <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
       <button onClick={onLogout}>로그아웃</button>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>menu scroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
       {children}
     </div>
   );
