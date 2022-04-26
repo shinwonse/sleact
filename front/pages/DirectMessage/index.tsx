a3a0600ce5a1c429f8be0461d84ace986db436e8
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-04-26 15:19:30 +0900

    디엠 수정

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -2,11 +2,12 @@ import Workspace from '@layouts/Workspace';
 import React from 'react';
 
 const DirectMessage = () => {
-  return (
-    <Workspace>
-      <div>dm</div>
-    </Workspace>
-  );
+  return null;
+  // return (
+  //   <Workspace>
+  //     <div>dm</div>
+  //   </Workspace>
+  // );
 };
 
 export default DirectMessage;
